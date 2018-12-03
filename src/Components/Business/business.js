d41190716905668d6da364b396279f922e0a68d0
Chan W. Park <eta.tauri@gmail.com>
2018-12-02 20:04:52 -0800

    second milestone

--- src/Components/Business/business.js
+++ src/Components/Business/business.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import './business.css';
 
-const business = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-};
+
 
 class Business extends React.Component {
     render() {
@@ -19,19 +9,19 @@ class Business extends React.Component {
             <div className = 'Business'>
                 <div className = 'image-container'>
                     <img
-                        src = {business.imageSrc}
-                        alt = {business.name} />
+                        src = {this.props.business.imageSrc}
+                        alt = {this.props.business.name} />
                 </div>
                 <h2>MarginOto Pizzeria</h2>
                 <div className = 'Business-information'>
                     <div className = 'Business-address'>
-                        <p>{business.address}</p>
-                        <p>{business.city}</p>
-                        <p>{business.state} {business.zipCode}</p>
+                        <p>{this.props.business.address}</p>
+                        <p>{this.props.business.city}</p>
+                        <p>{this.props.business.state} {this.props.business.zipCode}</p>
                     </div>
                     <div className = 'Business-reviews'>
-                        <h3>{business.category}</h3>
-                        <h3 className = 'rating'>{business.rating}</h3>
+                        <h3>{this.props.business.category}</h3>
+                        <h3 className = 'rating'>{this.props.business.rating}</h3>
                         <p>90 reviews</p>
                     </div>
                 </div>
